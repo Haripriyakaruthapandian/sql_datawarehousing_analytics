9cf9a912e43e6ffc59426805ebf91e65b72d7e07
Haripriya Karuthapandian <106544773+Haripriyakaruthapandian@users.noreply.github.com>
2025-05-02 12:10:13 +0530

    Update 10_part_to_whole_analysis.sql

--- Advanced_data_analytics/10_part_to_whole_analysis.sql
+++ Advanced_data_analytics/10_part_to_whole_analysis.sql
@@ -1,3 +1,18 @@
+/*
+===============================================================================
+Part-to-Whole Analysis
+===============================================================================
+Purpose:
+    - To compare performance or metrics across dimensions or time periods.
+    - To evaluate differences between categories.
+    - Useful for A/B testing or regional comparisons.
+
+SQL Functions Used:
+    - SUM(), AVG(): Aggregates values for comparison.
+    - Window Functions: SUM() OVER() for total calculations.
+===============================================================================
+*/
+
 --	WHICH CATEGORIES CONTRIBUTE THE MOST TO OVERALL SAles--
 with cat_range as (
 select p.category as category,
